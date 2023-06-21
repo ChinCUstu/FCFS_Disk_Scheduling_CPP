d90b5893bc23e156ffc074e3c8b7312578e948a3
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2023-06-21 09:25:48 +0100

    Add FCFS disk scheduling algorithm implementation
    This commit adds the implementation of the First-Come, First-Served (FCFS) disk scheduling algorithm in C++. The FCFS algorithm manages disk access requests based on their arrival order, optimizing seek times. By introducing this code, the project gains a fundamental disk scheduling algorithm, setting the stage for further enhancements and the inclusion of additional scheduling algorithms.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void fcfsDiskScheduling(const vector<int> &requests, int headPosition) {
+    int totalSeekTime = 0;
+    for (int request: requests) {
+        int seekDistance = abs(request - headPosition);
+        totalSeekTime += seekDistance;
+        headPosition = request;
+    }
+    cout << "Total seek time using FCFS: " << totalSeekTime << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> requests = {98, 183, 37, 122, 14, 124, 65, 67};
+    int headPosition = 53;
+
+    fcfsDiskScheduling(requests, headPosition);
     return 0;
 }
